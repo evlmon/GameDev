0462343caad609878c3054f045152d7520e623ef
evlmon <ciorbaandr3i@gmail.com>
2015-05-09 03:24:02 +0300

    Add shot bean

--- GameDev/Assets/Scripts/CharacterControlScript.cs
+++ GameDev/Assets/Scripts/CharacterControlScript.cs
@@ -8,15 +8,19 @@ public class CharacterControlScript : MonoBehaviour {
 	public float jumpForce = 10f;
 	public Transform groundCheck;
     public float CATCH_UP_SPEED = 3f;
+    public float BOLT_SPEED = 5;
+
+    public GameObject shot;
+    public Transform shotSpawn;
 
     private Vector2 originalPosition;
 	bool grounded = false;
 	float groundRadius = 0.2f;
-	bool touchingFloor = false;
 	float speed = 0f;
 
 	private Rigidbody2D rb2d;
 	private bool jump = false;
+
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
@@ -26,11 +30,21 @@ public class CharacterControlScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-		touchingFloor = Physics2D.OverlapCircle (groundCheck.position, groundRadius, floor);
-		if (Input.GetButtonDown("Jump") && (grounded || touchingFloor))
+		if (Input.GetButtonDown("Jump") && grounded)
 		{
 			jump = true;
 		}
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            var mouseClickPosition = Input.mousePosition;
+            mouseClickPosition = Camera.main.ScreenToWorldPoint(mouseClickPosition);
+            var xvelocityVector = (shotSpawn.position.x) - (mouseClickPosition.x);
+            var yvelocityVector = (shotSpawn.position.y) - (mouseClickPosition.y);
+            var rotation = Quaternion.FromToRotation(Vector3.up, shotSpawn.position - mouseClickPosition);
+            Rigidbody2D newShotrb2d = Instantiate(shot, shotSpawn.position, rotation) as Rigidbody2D;
+            newShotrb2d.velocity = new Vector2(1, xvelocityVector / yvelocityVector) * BOLT_SPEED;
+        }
 	}
 
 	void FixedUpdate()
---
+++ GameDev/Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+    
+    // Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    public void Move(Vector3 direction, Vector3 rotation)
+    {
+        //transform.rotation =Quaternion. rotation;
+        rb2d.velocity = direction;   
+    }
+}
